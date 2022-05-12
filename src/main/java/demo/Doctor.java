db9d98f2385a6f064c05ec6be35f1b5717265718
MEHUL KADAM <developer.mehul@gmail.com>
2022-05-12 18:23:01 +0530

    add bean config

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,10 +1,28 @@
 package demo;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope(scopeName = "prototype")
 @Component
 public class Doctor implements Staff {
+    private String qualification;
    public void assist(){
         System.out.println("Doctor is assisting");
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "qualification='" + qualification + '\'' +
+                '}';
     }
 }
